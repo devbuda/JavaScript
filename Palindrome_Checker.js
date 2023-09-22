42719ec7e8dd9c9eedee0755e978c3a2c3f97dd4
Victor Freire <69279495+devbuda@users.noreply.github.com>
2023-09-22 01:21:34 -0400

    Create Palindrome_Checker.js

---
+++ Palindrome_Checker.js
@@ -0,0 +1,10 @@
+function palindrome(str) {
+
+  str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+  return str === str.split('').reverse().join('');
+}
+console.log(palindrome("eye")); // true
+console.log(palindrome("RaceCar")); // true
+console.log(palindrome("2A3*3a2")); // true
+console.log(palindrome("hello")); // false
